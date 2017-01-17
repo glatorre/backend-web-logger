fa276b9daeb186c78e37dff340a3f68b84bd587a
Giuseppe La Torre <giuseppe.latorre@telecomitalia.it>
2017-01-17 17:07:13 +0100

    Removed appToken from config.json

--- client/index.js
+++ client/index.js
@@ -3,12 +3,21 @@ var config = require('./config.json');
 
 _log = console.log;
 
+var appToken;
+
+function Logger(_appToken){
+	appToken = _appToken;
+}
+
 console.log = function(msg){
 	request({
     	url: config.server.url + ':' + config.server.port + "/log",
     	method: "POST",
-    	json: {msg:msg, token:config.app.token}
+    	//json: {msg:msg, token:config.app.token}
+		json: {msg:msg, token:appToken}
     },function(error, response, body){
     	if(error){_log("error")};
     });
 }
+
+module.exports = Logger;
--- test/index.js
+++ test/index.js
@@ -1,4 +1,4 @@
-require("../client");
+new (require("../client"))("12345");
 
 setInterval(function(){
 	console.log("Prova: " + new Date().getTime());
